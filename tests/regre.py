a89b87311ec6ed499d314a510901beb2aa882e5f
fexcode <2734664632@qq.com>
2026-07-03 22:28:39 +0800

    fix: add missing EXPECT entry for regression test406.vix

--- tests/regre.py
+++ tests/regre.py
@@ -418,7 +418,7 @@ class TestExistingRegression:
         "test403.vix": (True, ["5", "7", "edge"]),
         "test404.vix": (True, ["1"]),
         "test405.vix": (True, ["1", "2", "2", "3", "1"]),
-    }
+        "test406.vix": (True, ["new", "values", "2"]),
 
     @pytest.mark.integration
     @pytest.mark.parametrize(
